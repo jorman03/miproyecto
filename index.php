99319da3c42f26bdd6f1031a0a70965faeb6b189
JORMAN . <jormanmoncada.estudiante@iser.edu.co>
2024-11-02 03:30:51 -0500

    primer commit myproyecto

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido al Fenómeno OVNI</title>
+    <style>
+        body {
+            margin: 0;
+            background: url('uploads/ezgif.com-video-to-gif-converter.gif') no-repeat center center fixed; /* Cambia esta URL a la imagen que prefieras */
+            background-size: cover;
+            font-family: Arial, sans-serif;
+            color: white;
+        }
+
+        .caja {
+            display: grid;
+            place-items: center;
+            min-height: 100vh;
+            text-align: center;
+            background-color: rgba(0, 0, 0, 0.5); /* Fondo semitransparente */
+            padding: 20px;
+        }
+
+        h1 {
+            font-size: 48px;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 24px;
+            line-height: 1.5;
+        }
+
+        .boton {
+            margin-top: 30px;
+            padding: 15px 30px;
+            background-color: #007BFF;
+            border: none;
+            color: white;
+            font-size: 18px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .boton:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <div class="caja">
+        <div class="contenido">
+            <h1>¡Bienvenido al Fenómeno OVNI!</h1>
+            <p>Explora los misterios del universo y descubre los secretos detrás de los objetos voladores no identificados. ¿Qué tan cerca estamos de conocer la verdad?</p>
+            <p>Acompáñanos en esta increíble travesía por lo desconocido.</p>
+            <a href="../mi_proyecto/views/login.php" class="boton">Comienza tu aventura</a>
+        </div>
+    </div>
+</body>
+</html>
